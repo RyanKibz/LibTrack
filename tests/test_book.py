3b021391ab2bf35054978e27ef2abb9f907d8481
RyanKibz <ryan.serem@students.moringaschool.com>
2026-09-11 14:04:55 +0300

    Post help from TM

---
+++ tests/test_book.py
@@ -0,0 +1,26 @@
+import pytest
+from models.book import Book
+
+
+def test_create_book():
+    book = Book(
+        "Things Fall Apart",
+        "Chinua Achebe",
+        "9780385474542",
+        "Fiction"
+    )
+
+    assert book.title == "Things Fall Apart"
+    assert book.author == "Chinua Achebe"
+    assert book.isbn == "9780385474542"
+    assert book.category == "Fiction"
+    assert book.availability == "Available"
+
+def test_book_requires_title():
+    with pytest.raises(ValueError):
+        Book(
+            "",
+            "Chinua Achebe",
+            "9780385474542",
+            "Fiction"
+        )
